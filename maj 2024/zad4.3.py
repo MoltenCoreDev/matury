ffefebc6c13c40fd44c7755a87db349392c12ee2
MoltenCoreDev <molters.tv@gmail.com>
2024-10-28 15:22:55 +0100

    sdfsd

--- maj 2024/zad4.3.py
+++ maj 2024/zad4.3.py
@@ -3,7 +3,7 @@ counter = 0
 first_row = []
 second_row = []
 
-with open("Dane/liczby.txt") as file:
+with open("Dane/liczby_przyklad.txt") as file:
     first_row = file.readline().split()
     first_row = [int(e) for e in first_row]
     second_row = file.readline().split()
@@ -18,3 +18,20 @@ def sort_arr(arr):
                 arr[j] = arr[j+1]
                 arr[j+1] = temp
     return arr
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i*i <= n:
+        if n % i:
+            i+= 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+for liczba in second_row:
+
